8ed8de3de4883feb94acf00d543cf9c8940f20e4
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-06-27 19:04:58 -0400

    added functions and variables

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,15 +1,42 @@
-const startGame = document.querySelector("button-container")
-const timeLeft = document.querySelector("timer")
-const wordInput = document.querySelector("word-container")
-const scoreInput = document.querySelector("score-container")
+const timeLeft = document.querySelector("#timer")
+const wordInput = document.querySelector("#word-container")
+const scoreInput = document.querySelector("#score-container")
 
-var time;
+
+const startGame = document.querySelector("#button-container")
+startGame.addEventListener("click", function() {
+    startGame.setAttribute("style", "display: none")
+    wordInput.style.visibility = "visible"
+    displayUnderscore()
+    countdown()
+});
+
+var time = 60;
 var intervalID;
-
-function startGuess() {
-
-}
+var answers;
+var currentWord;
 
 function displayUnderscore() {
-    
+ answers = ["_ _ _ _ _", "russell", "zula", "lola"]
+ let 
+ for (let i = 0; i < answers.length; i++) {
+   
+ }
+
 }
+function countdown() {
+    intervalID = setInterval(function name() {
+    time--; 
+    timeLeft.textContent = time;
+      if (time < 1) {
+        endGame()
+    } 
+    }, 1000);
+}
+
+function endGame() {
+    clearInterval(intervalID);
+    scoreInput.style.visibility = "visible"
+}
+
+
