e4677e780a6fa63ff428ee6301c6e9d9a37436d5
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-06-27 17:27:01 -0400

    added some js

---
+++ assets/javascript.js
@@ -0,0 +1,15 @@
+const startGame = document.querySelector("button-container")
+const timeLeft = document.querySelector("timer")
+const wordInput = document.querySelector("word-container")
+const scoreInput = document.querySelector("score-container")
+
+var time;
+var intervalID;
+
+function startGuess() {
+
+}
+
+function displayUnderscore() {
+    
+}
